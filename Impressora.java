825743f5896845d3bd4417381469b6df5c47252a
Weslley Rodrigues <weslley.rdgs@gmail.com>
2021-09-17 11:02:34 -0300

    Finalizado exercicio 2

--- Impressora.java
+++ Impressora.java
@@ -8,21 +8,21 @@ public class Impressora {
         public void exibir (float a, float b){
             System.out.println(a +","+ b);
         }
-        //1.3
+        //1.3   
         public void exibir (float b, String s){
             System.out.println(b +","+ s);
         }
         //1.4
         public void exibir (String s, float b){
-            System.out.println(b +","+ s);
+            System.out.println(s +","+ b);
         }
         //1.5 
-        public void exibir (String a, String b, String c){
-            System.out.println(a +","+ b + "," + c);
+        public void exibir (String s1, String s2, String s3){
+            System.out.println(s1 +","+ s2 + "," + s3);
         }
         //1.6
-        public void exibir (int a, int b, String c){
-            System.out.println(a +","+ b + "," + c);
+        public void exibir (int x1, int x2, String s1){
+            System.out.println(x1 +","+ x2 + "," + s1);
         }  
     }
 
--- TesteImpressora.java
+++ TesteImpressora.java
@@ -5,14 +5,20 @@ public class TesteImpressora {
 
         float a = 2.3f; 
         float b = 3.5f; 
-        String s = "s";
-        String aa = "a";
-        String bb = "b";
-        String cc = "c";
+        String s = "Ola float com string";
+        String s1 = "Ola";
+        String s2 = "Sequencia";
+        String s3 = "De tres Strings";
+        int x1 = 20;
+        int x2 = 21;
         
-        imp.exibir (a); 
-        imp.exibir (a,b); 
-        imp.exibir (a,b); 
+        imp.exibir (a); //1.1
+        imp.exibir (a,b); //1.2
+        imp.exibir (a, s); //1.3
+        imp.exibir (s, a); //1.4
+        imp.exibir (s1, s2, s3); //1.5
+        imp.exibir (x1, x2, s1); //1.6
+        
     }
 
 }
