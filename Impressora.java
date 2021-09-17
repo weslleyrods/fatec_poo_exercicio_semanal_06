59e1fc92ebbaea11338ac734ad8235653f6f2541
Weslley Rodrigues <weslley.rdgs@gmail.com>
2021-09-17 10:42:57 -0300

    Inicio dos  exercicios de Sobrecaarga

---
+++ Impressora.java
@@ -0,0 +1,28 @@
+public class Impressora {
+    
+        //1.1
+        public void exibir (float a){
+            System.out.println(a);
+        }
+        //1.2
+        public void exibir (float a, float b){
+            System.out.println(a +","+ b);
+        }
+        //1.3
+        public void exibir (float b, String s){
+            System.out.println(b +","+ s);
+        }
+        //1.4
+        public void exibir (String s, float b){
+            System.out.println(b +","+ s);
+        }
+        //1.5 
+        public void exibir (String a, String b, String c){
+            System.out.println(a +","+ b + "," + c);
+        }
+        //1.6
+        public void exibir (int a, int b, String c){
+            System.out.println(a +","+ b + "," + c);
+        }  
+    }
+
---
+++ TesteImpressora.java
@@ -0,0 +1,18 @@
+public class TesteImpressora {
+    public static void main(String[] args) {
+        
+        Impressora imp = new Impressora();
+
+        float a = 2.3f; 
+        float b = 3.5f; 
+        String s = "s";
+        String aa = "a";
+        String bb = "b";
+        String cc = "c";
+        
+        imp.exibir (a); 
+        imp.exibir (a,b); 
+        imp.exibir (a,b); 
+    }
+
+}
